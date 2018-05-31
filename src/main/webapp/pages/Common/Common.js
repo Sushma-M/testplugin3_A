5ae7d6ee9a473d9adbdcddf73eedbee11e54f83f
Sushma Wavemaker <sushma.maddirala@wavemaker.com>
2018-05-31 10:32:37 +0000

    ererer New files added - [AdminDash,CustomerRepo,CustomerRepoTest,Dashboard,EmployeeInfo,Leads,Locations,Page,Page1,SalesGrowth,SalesReps,aa,ep,leadsList,p,salesgrowthfilter,systemvitalsfilter,FileService,MyJavaService,MyJavaService1,MyJavaService2,MyJavaService3,MyJavaService4,MyJavaService5,MyTestDatabase,googleMap,googleapis,hrdb,salesdb,sp_insert_customer_repo_list,SalesForce,dataMapPrefab,googlemaps,youtube,Android,IOS,Other,data,flyer,fonts,imagelists,uploads,de.json,en.json,en.json,Android,IOS,Other,uploads] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,logos,profiles,resources,webapp,.gitignore,.wmproject.properties,pom.xml,pages-config.json,Readme.txt] Removed files - [fonts,material,Readme.txt]

--- src/main/webapp/pages/Common/Common.js
+++ src/main/webapp/pages/Common/Common.js
@@ -1,40 +1,30 @@
 Application.$controller("CommonPageController", ['$scope',
-    function ($scope) {
+    function($scope) {
         "use strict";
-        
-        /* perform any action on widgets/variables within this block */
-        $scope.onPageReady = function () {
+
+        /* perform any action with the variables inside this block(on-page-load) */
+        $scope.onPageVariablesReady = function() {
             /*
-             * variables can be accessed through '$scope.Variables' property here
-             * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-             * $scope.Variables.loggedInUser.getData()
-             * 
-             * widgets can be accessed through '$scope.Widgets' property here
-             * e.g. to get value of text widget named 'username' use following script
-             * '$scope.Widgets.username.datavalue'
+             * variables can be accessed through '$scope.Variables' service here
+             * e.g. $scope.Variables.staticVariable1.getData()
              */
         };
+
     }
 ]);
 
-Application.$controller("CommonLoginDialogController", ["$scope", "DialogService", "$window",
-    function ($scope, DialogService, $window) {
+Application.$controller("CommonLoginDialogController", ["$scope", "DialogService",
+    function($scope, DialogService) {
         "use strict";
         $scope.ctrlScope = $scope;
 
-        $scope.CommonLoginDialogError = function ($event, $isolateScope) {
+        $scope.CommonLoginDialogError = function($event, $isolateScope) {
             /*
-             * Error message can be accessed from the property $isolateScope.loginMessage.caption
+             * Error message can be accessed from the property $isolateScope.errMsg
              */
         };
 
-        $scope.CommonLoginDialogSuccess = function ($event, $isolateScope) {
-            /*
-             * This success handler provides a redirectUrl which is the role based url set while configuring Security service for the project.
-             * The redirectUrl can be accessed as $isolateScope.redirectUrl
-             * To navigate to the url use '$window' service as:
-             * $window.location = $isolateScope.redirectUrl
-             */
+        $scope.CommonLoginDialogSuccess = function($event, $isolateScope) {
             DialogService.hideDialog("CommonLoginDialog");
         };
     }
---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "defaultLanguage" : "en",
+  "displayName" : "testplugin3",
+  "homePage" : "Main",
+  "name" : "testplugin3",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
